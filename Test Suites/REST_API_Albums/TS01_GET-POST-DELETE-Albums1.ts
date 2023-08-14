ea0179b89b1b5b001edad5aa8a53ef167712869f
Najwa Kariza <9g.najwakariza@gmail.com>
2023-08-14 10:38:31 +0700

    refactor: add albums (1st endpoint) test suites

---
+++ Test Suites/REST_API_Albums/TS01_GET-POST-DELETE-Albums1.ts
@@ -0,0 +1,35 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS01_GET-POST-DELETE-Albums1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>b093603c-52fe-460d-adc3-476a9707b3c0</testSuiteGuid>
+   <testCaseLink>
+      <guid>92c7a661-05ef-4e74-b684-9b1e13cbadf2</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/REST_API_Albums/TC01_GET-All-Albums</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>439ca8e2-9acb-4056-9a45-d58b9a2fe41b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/REST_API_Albums/TC03_POST-Albums</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>61da2476-7961-4374-b34f-693a879523c1</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/REST_API_Albums/TC04_DELETE-Albums</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
