8a6fca16ddbc9d5480c2d9be4a1f24ff8b57f2d7
Najwa Kariza <9g.najwakariza@gmail.com>
2023-08-14 16:03:03 +0700

    refactor: add todos (2nd endpoint) test suites

---
+++ Test Suites/REST_API_ToDos/TS02_GET-POST-DELETE-ToDos.ts
@@ -0,0 +1,35 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS02_GET-POST-DELETE-ToDos</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>7255688f-b813-4cdc-b807-4f9d3cb3b4b4</testSuiteGuid>
+   <testCaseLink>
+      <guid>1bfc2510-bed5-4108-a792-88bc652dabac</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/REST_API_ToDos/GET/TC01_GET-All-ToDos</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>eee632ce-94fd-4480-b7b8-564b51e18a18</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/REST_API_ToDos/POST/TC12_POST-ToDos</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>3f07e42a-7148-4003-be5d-56fa7a59d4e2</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/REST_API_ToDos/DELETE/TC13_DELETE-ToDos</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
