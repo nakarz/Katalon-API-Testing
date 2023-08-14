e9a721366cc5edde030200633e72a9184e9dac4c
Najwa Kariza <9g.najwakariza@gmail.com>
2023-08-14 16:04:29 +0700

    refactor: rename album test suite

--- Test Suites/REST_API_Albums/TS01_GET-POST-DELETE-Albums.ts
+++ Test Suites/REST_API_Albums/TS01_GET-POST-DELETE-Albums.ts
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <TestSuiteEntity>
    <description></description>
-   <name>TS01_GET-POST-DELETE-Albums1</name>
+   <name>TS01_GET-POST-DELETE-Albums</name>
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
